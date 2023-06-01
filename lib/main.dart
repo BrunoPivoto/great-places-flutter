fbe53011d935a424f9f78ad04e08ad62d8cccb98
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-06-01 11:40:56 -0300

    add addFunction, Provider and pathProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/screens/place_form_screen.dart';
 import 'package:great_places/screens/places_list_screen.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,15 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: theme.copyWith(
-        colorScheme: theme.colorScheme.copyWith(secondary: Colors.amber, primary: Colors.indigo),
+    return ChangeNotifierProvider(
+      create: (ctx) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(secondary: Colors.amber, primary: Colors.indigo),
+        ),
+        home: const PlacesListScreen(),
+        routes: {
+          AppRoutes.placeForm: (ctx) => const PlaceFormScreen(),
+        },
       ),
-      home: const PlacesListScreen(),
-      routes: {
-        AppRoutes.placeForm: (ctx) => const PlaceFormScreen(),
-      },
     );
   }
 }
