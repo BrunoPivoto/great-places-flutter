78c063ea18dfd386edfaabee3091db2f039cef36
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-06-01 13:09:41 -0300

    place_list

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({super.key});
@@ -7,18 +9,31 @@ class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meus Lugares'),
-        actions: [
-          IconButton(
-            onPressed: () => Navigator.of(context).pushNamed(AppRoutes.placeForm),
-            icon: const Icon(Icons.add),
+        appBar: AppBar(
+          title: const Text('Meus Lugares'),
+          actions: [
+            IconButton(
+              onPressed: () => Navigator.of(context).pushNamed(AppRoutes.placeForm),
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        body: Consumer<GreatPlaces>(
+          builder: (ctx, greatPlaces, child) => greatPlaces.itemsCount == 0
+              ? child!
+              : ListView.builder(
+                  itemCount: greatPlaces.itemsCount,
+                  itemBuilder: (ctx, index) => ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: FileImage(greatPlaces.itemByIndex(index).image),
+                    ),
+                    title: Text(greatPlaces.itemByIndex(index).title),
+                    onTap: () {},
+                  ),
+                ),
+          child: const Center(
+            child: Text('Nenhum local cadastrado'),
           ),
-        ],
-      ),
-      body: Center(
-        child: CircularProgressIndicator(color: Theme.of(context).colorScheme.secondary),
-      ),
-    );
+        ));
   }
 }
